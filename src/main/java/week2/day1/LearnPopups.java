a0224e7d402f9cf2f06d41741ee6778f4f4a61a8
Sethu Mathavan Subramanian <sethutks@gmail.com>
2017-11-27 12:22:13 +0530

    Pop up Windows Handling

---
+++ src/main/java/week2/day1/LearnPopups.java
@@ -0,0 +1,47 @@
+package week2.day1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class LearnPopups {
+	
+	@Test
+	public void testPopups() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.get("http://legacy.crystalcruises.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElementByLinkText("GUEST CHECK-IN").click();
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		Set<String> allWindows = driver.getWindowHandles();
+		List<String> allWindowsList = new ArrayList<String>();
+		allWindowsList.addAll(allWindows);
+		System.out.println(allWindows.size());
+		String parentWindow = driver.getWindowHandle();
+		String childWindow = allWindowsList.get(1);
+		driver.switchTo().window(childWindow);
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		driver.switchTo().window(parentWindow);
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
